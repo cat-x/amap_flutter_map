ce1b7b2f94732a679f340e1a07be78ee7e8c5677
cat-x <a599231042@163.com>
2023-11-07 17:35:28 +0800

    add ios ClusterAnnotationView

---
+++ ios/Classes/View/ClusterAnnotationView.h
@@ -0,0 +1,19 @@
+//
+//  ClusterAnnotationView.h
+//  officialDemo2D
+//
+//  Created by yi chen on 14-5-15.
+//  Copyright (c) 2014年 AutoNavi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MAMapKit/MAMapKit.h>
+
+
+@interface ClusterAnnotationView : MAAnnotationView
+
+@property (nonatomic, assign) NSUInteger count;
+
+- (id)initWithAnnotation:(id<MAAnnotation>)annotation reuseIdentifier:(NSString *)reuseIdentifier useIcon:(BOOL)useIcon;
+
+@end
